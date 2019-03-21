985a74d21c98bb62fe73db706a22401679e6c960
Samsool <48785808+Samsool@users.noreply.github.com>
2019-03-21 09:27:48 +0100

    modif1

---
+++ Run.java
@@ -0,0 +1,8 @@
+public class Run{
+	public static void main(String[] args) {
+		String instrument = "hautbois";
+		double frequence = 520.0;
+		Synthesis s = new Synthesis (instrument, frequence);
+		Unique u = new Unique (instrument, frequence);
+	}
+}
