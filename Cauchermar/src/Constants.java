385cf6b00804d803b95e3f2216a931e87f3e5326
Emma <emmaneiss2@gmail.com>
2019-04-18 22:09:33 +0200

    Simple et efficace

--- Cauchermar/src/Constants.java
+++
@@ -1,6 +0,0 @@
-public class Constants {
-
-    static final int FLUTEDEPAN = 0,  FLUTEABEC = 1, CLARINETTE = 2, HAUTBOIS = 3, ORGUE = 4;
-    static final String[] STRINGS = {"Pan_Flute", "Flute", "Clarinet", "Oboe","Church_Organ"};
-
-}
